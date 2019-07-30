8036061d904c0885e6ffc310543d5330917d51dd
Illio, the Great <pwfcoding@gmail.com>
2019-07-30 09:34:02 -0500

    creation of triviaQuestions variable and array

---
+++ assets/javascript/app.js
@@ -0,0 +1,78 @@
+var triviaQuestions = [{
+
+    question: "",
+    possibleAnswers: [],
+    answer: 0
+},
+
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 0
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 1
+
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 3
+},
+
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 2
+
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 2
+
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 2
+
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 2
+
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 2
+
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 2
+
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 2
+
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 2
+
+},
+{
+    question: "",
+    possibleAnswers: [],
+    answer: 2
+
+}];
